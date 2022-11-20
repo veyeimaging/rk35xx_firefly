db47b7a1b60661cc1025f7c5ff85de96d8e97a68
xumm <xumm@csoneplus.com>
2022-11-20 10:39:42 +0800

    add preview.sh

---
+++ linux/sample/preview.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DISPLAY=:0
+export XAUTHORITY=/home/firefly/.Xauthority
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/aarch64-linux-gnu/gstreamer-1.0
+WIDTH=1920
+HEIGHT=1080
+SINK=xvimagesink
+
+gst-launch-1.0 v4l2src device=/dev/video0 ! video/x-raw,format=NV12,width=${WIDTH},height=${HEIGHT}, framerate=30/1 ! videoconvert ! $SINK&
+
+wait
