6f8082fd022d6f7a52d43d6e378e7fd655b3a332
xumm <xumm@csoneplus.com>
2023-04-14 10:03:11 +0800

    add sample for：mv camera support import to opencv and preview

---
+++ linux/samples/opencv/mv camera/v4l2dev_2_opencv_show_grey.py
@@ -0,0 +1,30 @@
+import sys
+import argparse
+import subprocess
+import cv2
+
+def read_cam(width, height, fps):
+    
+    v4l2_cmd = f'media-ctl -d /dev/media0 --set-v4l2 \'"m00_b_mvcam 7-003b":0[fmt:Y8_1X8/{width}x{height}@1/{fps} field:none]\''
+    subprocess.run(v4l2_cmd, shell=True)
+    
+    cap = cv2.VideoCapture(f"v4l2src io-mode=dmabuf device=/dev/video0 ! video/x-raw, format=(string)GRAY8, width=(int){width}, height=(int){height} ! appsink")
+    if cap.isOpened():
+        cv2.namedWindow("demo", cv2.WINDOW_AUTOSIZE)
+        while True:
+            ret_val, img = cap.read();
+            cv2.imshow('demo',img)
+            cv2.waitKey(1)
+    else:
+     print ("camera open failed");
+
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Read camera video stream')
+    parser.add_argument('--width', type=int, default=1080, help='width of the video stream')
+    parser.add_argument('--height', type=int, default=1080, help='height of the video stream')
+    parser.add_argument('--fps', type=int, default=30, help='fps of the video stream')
+    args = parser.parse_args()
+
+    read_cam(args.width, args.height, args.fps)
